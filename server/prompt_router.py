dc9eccc418a7e13df50676d8c1b0737471e9cec5
jochen ade <jochen.ade@gmail.com>
2023-04-23 07:46:49 +0900

    history now working for multiple users

--- server/prompt_router.py
+++ server/prompt_router.py
@@ -1,47 +1,50 @@
-from fastapi import APIRouter, Body, HTTPException
+from fastapi import APIRouter, Request, Body, HTTPException
 from typing import Dict
 import openai
 
 prompt_router = APIRouter()
 
-# Add a global variable to store conversation history
-conversation_history = [
-    {"role": "system", "content": "You are a helpful assistant."}
-]
+# Store conversation histories in a dictionary
+conversation_histories = {}
 
 # Add a constant for maximum memory tokens
 MAX_MEMORY_TOKENS = 70
 
 @prompt_router.post("/prompt")
-async def chat_endpoint(body: Dict[str, str] = Body(...)):
+async def chat_endpoint(user_id: str, body: Dict[str, str] = Body(...)):
+    # Check if user_id exists in conversation_histories, if not, create a new history
+    if user_id not in conversation_histories:
+        conversation_histories[user_id] = [{"role": "system", "content": "You are a helpful assistant."}]
+
     user_message = body.get("user_message")
     if not user_message:
         raise HTTPException(status_code=400, detail="user_message is required")
 
-    conversation_history.append({"role": "user", "content": user_message})
+    conversation_histories[user_id].append({"role": "user", "content": user_message})
 
     response = openai.ChatCompletion.create(
         model="gpt-4", # OR gpt-3.5-turbo
-        messages=conversation_history,
+        messages=conversation_histories[user_id],
     )
 
     assistant_message = response.choices[0].message["content"]
-    conversation_history.append({"role": "assistant", "content": assistant_message})
+    conversation_histories[user_id].append({"role": "assistant", "content": assistant_message})
 
     # Remove older messages when total tokens in conversation_history exceed MAX_MEMORY_TOKENS
     while response.usage['total_tokens'] > MAX_MEMORY_TOKENS:
         # Ensure that there are at least two messages (one "system" and one other message) before removing a message
-        if len(conversation_history) > 2:
-            removed_message = conversation_history.pop(1)  # Skip the "system" message at index 0
+        if len(conversation_histories[user_id]) > 2:
+            removed_message = conversation_histories[user_id].pop(1)  # Skip the "system" message at index 0
             # Create a new API call without the removed_message
             response = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo",
-                messages=conversation_history,
+                messages=conversation_histories[user_id],
             )
         else:
             break
 
     return assistant_message
+
 
 
 
